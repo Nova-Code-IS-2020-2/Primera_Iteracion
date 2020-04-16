6d9a7b3c96132acaf3368eae6cfdda0d698f3e23
GisselleIb <gisselleib@ciencias.unam.mx>
2020-04-15 19:32:34 -0500

    Ya se pueden crear y guardar categorías a la base de datos

--- DeliExpress/src/com/deliexpress/beans/Categoria.java
+++ DeliExpress/src/com/deliexpress/beans/Categoria.java
@@ -5,12 +5,19 @@ public class Categoria {
 	private int id_cat;
 	private String nombre_cat;
 	
-	public int getId() {
-		return id_cat;
+	public Categoria() {
 	}
 	
-	public void setId(int id) {
-		id_cat=id;
+	public Categoria(String nombre_cat) {
+		this.nombre_cat=nombre_cat;
+	}
+	
+	public int getId() {
+		return this.id_cat;
+	}
+	
+	public void setId(int id_cat) {
+		this.id_cat=id_cat;
 	}
 	
 	public String getName(){
--- DeliExpress/src/com/deliexpress/controller/CategoriaController.java
+++ DeliExpress/src/com/deliexpress/controller/CategoriaController.java
@@ -1,54 +1,41 @@
 package com.deliexpress.controller;
-
-import java.util.List;    
-import org.springframework.beans.factory.annotation.Autowired;    
-import org.springframework.stereotype.Controller;  
-import org.springframework.ui.Model;  
-import org.springframework.web.bind.annotation.ModelAttribute;    
-import org.springframework.web.bind.annotation.PathVariable;    
-import org.springframework.web.bind.annotation.RequestMapping;    
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import com.deliexpress.beans.Categoria;
-import com.deliexpress.dao.CatDao;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class CategoriaController {
+import com.deliexpress.beans.Categoria;
+import com.deliexpress.dao.CategoriaDAO;
+
+import java.util.List; 
+import java.io.*;
+@Controller
+public class CategoriaController{
 	
-	CatDao dao;
-	
-	/*Muestra un formulario para introducir informacion,"command" es un atributo de solicitud  
-    * reservado que se se usa para mostrar la información del objeto en un formulario  
-    */   
-	@RequestMapping("/catform")
-	public String showForm(Model m) {
-		m.addAttribute("command", new Categoria());  
-        return "catform";   
+	@Autowired
+    private CategoriaDAO categoriaDAO;
+ 
+	@RequestMapping(value="/menuAdmin")
+	public ModelAndView listaCategoria(ModelAndView model) throws IOException{
+	    List<Categoria> listCat = categoriaDAO.list();
+	    model.addObject("listCat", listCat);
+	    model.setViewName("menuAdmin");
+	 
+	    return model;
 	}
-	
-	@RequestMapping(value="/save",method = RequestMethod.POST)    
-    public String save(@ModelAttribute("cat") Categoria categoria){    
-        dao.save(categoria);    
-        return "redirect:/leerMenuAdmin";//will redirect to viewcat request mapping    
-    }   
-	
-	 /* It displays object data into form for the given id.   
-     * The @PathVariable puts URL data into variable.*/    
-    @RequestMapping(value="/editcat/{id}")    
-    public String edit(@PathVariable int id, Model m){    
-        Categoria cat=dao.getCatById(id);    
-        m.addAttribute("command",cat);  
-        return "cateditform";    
-    }    
-    /* It updates model object. */    
-    @RequestMapping(value="/editsave",method = RequestMethod.POST)    
-    public String editsave(@ModelAttribute("cat") Categoria cat){    
-        dao.update(cat);    
-        return "redirect:/leerMenuAdmin";    
-    }    
-    /* It deletes record for the given id in URL and redirects to /viewcat */    
-    @RequestMapping(value="/deletecat/{id}",method = RequestMethod.GET)    
-    public String delete(@PathVariable int id){    
-        dao.delete(id);    
-        return "redirect:/leerMenuAdmin";    
-    }     
-	
+	@RequestMapping(value = "/agregarCategoria", method = RequestMethod.GET)
+	public ModelAndView agregarCategoria(ModelAndView model) {
+	    Categoria categoria = new Categoria();
+	    model.addObject("categoria", categoria);
+	    model.setViewName("catform");
+	    return model;
+	}
+	@RequestMapping(value = "/guardarCategoria", method = RequestMethod.POST)
+	public ModelAndView guardarCategoria(@ModelAttribute Categoria categoria) {
+	    categoriaDAO.save(categoria);
+	    return new ModelAndView("redirect:/menuAdmin");
+	}
 }
+
