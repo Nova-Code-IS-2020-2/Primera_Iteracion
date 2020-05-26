0926ddfcb8cf25ff76dd454b986a8be0e461d4ac
Gisselle <gisselleib@ciencias.unam.mx>
2020-05-25 21:15:00 -0500

    Problema de la pantalla de las ordenes del administrador arreglado.

--- DeliExpress/src/com/deliexpress/controller/OrdenController.java
+++ DeliExpress/src/com/deliexpress/controller/OrdenController.java
@@ -44,7 +44,12 @@ public class OrdenController {
 			 try {
 				 IniciarSesionDAO isDAO = new IniciarSesionDAO();
 				 List<String[]> dirs = isDAO.dirIdOrd();
-				 ModelAndView mav=new ModelAndView("seleccionarcomida");
+				 ModelAndView mav = null;
+				 if (s.getAttribute("admin") == null) {
+					 mav=new ModelAndView("seleccionarcomida");
+				 }else {
+					 mav=new ModelAndView("seleccionarcomidaAdmin");
+				 }
 				 mav.addObject("dirs", dirs);
 				 return mav;
 			 }catch (SQLException | ClassNotFoundException ex) {
