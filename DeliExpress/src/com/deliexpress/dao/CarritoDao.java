6ab4fb5ab73657c0bc793b174e9c6259e37363e7
Gisselle <gisselleib@ciencias.unam.mx>
2020-05-29 16:08:19 -0500

    Probado y sin errores

--- DeliExpress/src/com/deliexpress/dao/CarritoDao.java
+++ DeliExpress/src/com/deliexpress/dao/CarritoDao.java
@@ -36,9 +36,6 @@ public class CarritoDao {
 		        public Carrito mapRow(ResultSet rs, int rowNum) throws SQLException {
 				 	
 				 	Carrito aCat = new Carrito();
-				 	System.out.println("mapRow");
-				 	
-				 	
 		            aCat.setPrecio(rs.getFloat("precio"));
 		            aCat.setNombre(rs.getString("nombre_alim"));
 		            aCat.setCantidad(rs.getInt("cantidad"));
